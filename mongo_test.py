bf8c12a69bba8953e684ad7fe2373a809da4617d
macius092 <maciej.skorski092@gmail.com>
2020-06-11 20:51:51 +0200

    Data base infterface created

--- mongo_test.py
+++ mongo_test.py
@@ -1,13 +1,28 @@
-import pymongo
 from pymongo import MongoClient
+from constants import URL
 
-cluster = pymongo.MongoClient("mongodb+srv://testUser:kajamaria%4020170814@cluster0-cywqs.mongodb.net/test?retryWrites=true&w=majority")
-db = cluster.test
-db = cluster["test"]
-collection = db["test"]
 
-post = {
-    "_id": 0,
-    "name": 'maciej'
-}
-collection.insert_one(post)
+class MyMongo:
+    def __init__(self, url, db_name):
+        self.db = MongoClient(url)[db_name]
+
+    def let_me_udpdate(self, collection, data, id):
+        self.db[collection].update_one(
+            {"_id": id},
+            {"$set": data},
+        )
+
+    def let_me_insert(self, collection, data):
+        self.db[collection].insert_one(data)
+
+    def let_me_delete(self, collection, pointer):
+        self.db[collection].delete_one(pointer)
+
+    def let_me_find_one(self, collection, pointer):
+        return self.db[collection].find_one(pointer)
+
+    def let_me_find_all(self, collection, pointer):
+        return self.db[collection].find(pointer)
+
+
+db_obj = MyMongo(URL, "university")
