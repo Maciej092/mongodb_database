e1a6b0e4daad3a55627c67efb4d79566f29083c6
macius092 <maciej.skorski092@gmail.com>
2020-06-13 14:00:47 +0200

    Create database init schemas-update id

--- data_base_init.py
+++ data_base_init.py
@@ -8,35 +8,35 @@ class DataBase:
         db = ServerProxy(self.db_name)
 
         student_1 = db.let_me_insert([Collections.COLLECTION_STUDENT, {
-            "student_id": 123456,
+            "_id": 123456,
             "first_name": "Jan",
             "last_name": "Kowalski",
             "schema_version": "1"
         }])
 
         employee_1 = db.let_me_insert([Collections.COLLECTION_EMPLOYEE, {
-            "employee_id": 121212,
+            "_id": 121212,
             "first_name": "John",
             "last_name": "Smith",
             "schema_version": "1"
         }])
 
         classroom_1 = db.let_me_insert([Collections.COLLECTION_CLASSROOM, {
-            "classroom_id": 1,
+            "_id": 1,
             "name": "B5-601",
             "description": "laboratory room",
             "schema_version": "1"
         }])
 
         subject_1 = db.let_me_insert([Collections.COLLECTION_SUBJECT, {
-            "subject_id": 1,
+            "_id": 1,
             "name": "Rocket science",
             "employee_id": 121212,
             "schema_version": "1"
         }])
 
         subject_group_1 = db.let_me_insert([Collections.COLLECTION_SUBJECT_GROUP, {
-            "subject_group_id": 1,
+            "_id": 1,
             "group_name": "Grupa 1",
             "employee_id": 121212,
             "subject_id": 123456,
@@ -44,7 +44,7 @@ class DataBase:
         }])
 
         classes_1 = db.let_me_insert([Collections.COLLECTION_CLASSES, {
-            "classes_id": 1,
+            "_id": 1,
             "class_no": 5,
             "date_time": "2020-08-22",
             "classroom_id": 1,
@@ -53,14 +53,14 @@ class DataBase:
         }])
 
         assignment_1 = db.let_me_insert([Collections.COLLECTION_ASSIGNMENT, {
-            "assignment_id": 1,
+            "_id": 1,
             "student_id": 123456,
             "subject_id": 1,
             "grade": 5
         }])
 
         student_group_1 = db.let_me_insert([Collections.COLLECTION_STUDENT_GROUP, {
-            "student_id": 123456,
+            "_id": 123456,
             "subject_group_id": 1
         }])
 
